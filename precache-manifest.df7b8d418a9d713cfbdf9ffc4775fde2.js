937b97143933ca8ad9907ce79ec61d0aa5038cc5
estebanzarate <esteban-zarate@hotmail.com>
2022-07-12 13:41:16 -0300

    Updates

---
+++ precache-manifest.df7b8d418a9d713cfbdf9ffc4775fde2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4703044fc062684b8ce80c6812d2e5f",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "7a8b3967a152d86fef52",
+    "url": "/birthday-reminder/static/css/main.5c9d6d9d.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8b3967a152d86fef52",
+    "url": "/birthday-reminder/static/js/main.eded6bac.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
